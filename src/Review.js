fb22651d3266233b6d6c1f41e840ff4878b912c8
Dale Bendixen <dalebendixen@gmail.com>
2022-07-21 08:08:46 -0600

    completed project

--- src/Review.js
+++ src/Review.js
@@ -3,7 +3,72 @@ import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
-  return <h2>review component</h2>;
+  // set state
+  const [index, setIndex] = useState(0);
+  // destructure the people coming from data.js
+  const {name, job, image, text} = people[index];
+
+// function to avoid errors if reach end of array
+const checkNumber = (number) => {
+  if(number > people.length -1) {
+    return 0;
+  }
+  if(number < 0) {
+    return people.length -1;
+  }
+  return number;
+}
+
+
+
+  // create onClick event handlers
+  const nextPerson = () => {
+    setIndex((index) => {
+      let newIndex = index + 1;
+      return checkNumber(newIndex);
+    });
+  }
+  const prevPerson = () => {
+    setIndex((index) => {
+      let newIndex = index - 1;
+      return checkNumber(newIndex);
+    });
+  }
+
+  const randomPerson = () => {
+    let randomNumber = Math.floor(Math.random() * people.length)
+    if(randomNumber === index){
+      randomNumber = index + 1;
+    }
+    setIndex(checkNumber(randomNumber));
+    console.log(randomNumber)
+  }
+
+
+  return (
+    <article className='review'>
+      <div className="img-container">
+        <img src={ image } alt={ name } className='person-img' />
+        <span className='quote-icon' >
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className='author'>{name}</h4>
+      <p className='job'>{job}</p>
+      <p className="info">{ text }</p>
+      <div className="button-container"> 
+        <button className='prev-btn' onClick={prevPerson}>
+          <FaChevronLeft />
+        </button>
+        <button className='next-btn' onClick={nextPerson}>
+          <FaChevronRight />
+        </button>
+      </div>
+        <button className='random-btn'onClick={randomPerson}>
+          surprise me
+        </button>
+    </article>
+  )
 };
 
 export default Review;
